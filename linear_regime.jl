cf7593523a114fc97155211839edd74cba06d1c4
ep670 <ep670@cam.ac.uk>
2023-02-24 09:18:05 +0000

    FractSolid fit to repeated cycles
    Error: 0.0031826757770975524

--- linear_regime.jl
+++ linear_regime.jl
@@ -1,6 +1,7 @@
 using RHEOS
 using PyPlot
 using Plots
+using DataFrames
 
 ###############################################################################################################################
 ###################################### Fit experimental data to Fractional Solid ##############################################
@@ -28,7 +29,7 @@ ax.legend()
 ################################# Down-sample experimental data and re-fit to Fractional Solid ################################
 ###############################################################################################################################
 
-data_resampled = resample(data, scale = 0.2)
+data_resampled = resample(data, scale = 0.1)
 println(length(data_resampled.t)) #expected 113
 
 fract_solid_resampled = modelfit(data_resampled, FractSolid, strain_imposed, lo=(η = 0.0001, cᵦ = 0.0001, β = 0.0001, k = 0.0001), hi=(η = 10000, cᵦ = 10000, β = 10000, k = 10000), verbose=true)
@@ -47,15 +48,13 @@ ax.set_ylabel("Stress, Strain")
 ax.legend()
 
 ###############################################################################################################################
-####################################################### Three cycles ############################################################
+####################################################### All cycles ############################################################
 ###############################################################################################################################
 
-dc_data = importcsv("C:/Users/eleni/OneDrive/Documents/MRes_Sensor_CDT/Mini_Project/cycling/22-03-01_m4/double_cycle.csv", t_col=7, ϵ_col=6, σ_col=9)
+dc_data = importcsv("C:/Users/eleni/OneDrive/Documents/MRes_Sensor_CDT/Mini_Project/cycling/22-03-01_m4/all_cycles.csv", t_col=7, ϵ_col=6, σ_col=9)
 dc_data_resampled = resample(dc_data, scale = 0.2)
-println(length(dc_data.t)) #1788
-println(length(dc_data_resampled.t)) #expected 358
 
-fract_solid_dc = modelfit(dc_data_resampled, FractSolid, strain_imposed, lo=(η = 0.0001, cᵦ = 0.0001, β = 0.0001, k = 0.0001), hi=(η = 10000, cᵦ = 10000, β = 10000, k = 10000), verbose=true)
+fract_solid_dc = modelfit(dc_data_resampled, FractSolid, strain_imposed, lo=(η = 0.0001, cᵦ = 0.0001, β = 0.0001, k = 0.0001), hi=(η = 10000, cᵦ = 10000, β = 10000, k = 10000), verbose=true, rel_tol=1e-6)
 fractsolid_predict_dc = modelpredict(extract(dc_data_resampled, strain_only), fract_solid_dc)
 
 pygui(true)
@@ -65,27 +64,38 @@ ax.plot(dc_data_resampled.t, dc_data_resampled.ϵ, "--", label="Strain (Data)")
 ax.plot(dc_data_resampled.t, dc_data_resampled.σ, ".", label="Stress (Data)")
 ax.plot(fractsolid_predict_dc.t, fractsolid_predict_dc.σ, label="Stress (Predicted)")
 
-ax.set_title("Fractional Solid Triple Cycle Fit")
+ax.set_title("Fractional Solid Fit")
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Stress, Strain")
 ax.legend()
+
+#Time: 9207.7284172 s, Why: XTOL_REACHED, Parameters: [2.125555869960186, 1.633632673867637, 0.0001, 0.2760957120720441], Error: 0.0031826757770975524
 
 
 ###############################################################################################################################
 ############################## Fit undersampled experimental data to Standard Linear Solid ####################################
 ###############################################################################################################################
 
-sls_resampled = modelfit(data_resampled, SLS_Zener, strain_imposed,  verbose=true)
-sls_predict_resampled = modelpredict(extract(data_resampled, strain_only), sls_resampled)
+dc_data = importcsv("C:/Users/eleni/OneDrive/Documents/MRes_Sensor_CDT/Mini_Project/cycling/22-03-01_m4/all_cycles.csv", t_col=7, ϵ_col=6, σ_col=9)
+dc_data_resampled = resample(dc_data, scale = 0.2)
+
+sls_resampled = modelfit(dc_data_resampled, SLS_Zener, strain_imposed,  verbose=true, rel_tol=1e-6)
+sls_predict_resampled = modelpredict(extract(dc_data_resampled, strain_only), sls_resampled)
 
 pygui(true)
 fig, ax = subplots()
 
-ax.plot(data_resampled.t, data_resampled.ϵ, "--", label="Strain (Data)")
-ax.plot(data_resampled.t, data_resampled.σ, ".", label="Stress (Data)")
+ax.plot(dc_data_resampled.t, dc_data_resampled.ϵ, "--", label="Strain (Data)")
+ax.plot(dc_data_resampled.t, dc_data_resampled.σ, ".", label="Stress (Data)")
 ax.plot(sls_predict_resampled.t, sls_predict_resampled.σ, label="Stress (Predicted)")
 
 ax.set_title("Standard Linear Solid Fit")
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Stress, Strain")
+ax.legend()
+
+#Result: Time: 3.1867916 s, Why: XTOL_REACHED, Parameters: [2.113667257378493, 1.5586247109007703, 0.2758070503146254], Error: 0.003193660629536194
+
+#optimiser_list = [:LN_COBYLA,:LN_SBPLX,:LN_BOBYQA]
+#Can add optmethod = "" in modelfit to change optimised method
 
